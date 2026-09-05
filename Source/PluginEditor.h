c187577fcab5789b0ffcba654cacef6c535342de
jacksonwboyd <iamjackboyd@gmail.com>
2026-09-05 14:37:43 -0700

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,7 +2,8 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class PhysicalDrumEngineAudioProcessorEditor final : public juce::AudioProcessorEditor
+class PhysicalDrumEngineAudioProcessorEditor final : public juce::AudioProcessorEditor,
+                                                     public juce::DragAndDropTarget
 {
 public:
     explicit PhysicalDrumEngineAudioProcessorEditor(PhysicalDrumEngineAudioProcessor&);
@@ -10,6 +11,12 @@ public:
 
     void paint(juce::Graphics&) override;
     void resized() override;
+
+    bool isInterestedInFileDrag(const juce::StringArray& files) override;
+    void itemDragEnter(const juce::DragAndDropTarget::SourceDetails& dragSourceDetails) override;
+    void itemDragMove(const juce::DragAndDropTarget::SourceDetails& dragSourceDetails) override;
+    void itemDragExit(const juce::DragAndDropTarget::SourceDetails& dragSourceDetails) override;
+    void filesDropped(const juce::StringArray& files, int x, int y) override;
 
 private:
     PhysicalDrumEngineAudioProcessor& processor;
@@ -20,6 +27,8 @@ private:
     std::array<juce::Label, PhysicalDrumEngineAudioProcessor::numPads> padLabels;
     std::array<juce::Slider, 11> knobs;
     std::array<juce::Label, 11> knobLabels;
+    std::array<juce::Rectangle<int>, PhysicalDrumEngineAudioProcessor::numPads> padDropBounds{};
+    int dragPad = -1;
 
     static constexpr std::array<const char*, 11> knobIds = {
         "physicality", "transient", "attack", "brightness", "pitch", "body",
@@ -30,6 +39,9 @@ private:
         "PHYSICALITY", "TRANSIENT", "ATTACK", "BRIGHTNESS", "PITCH", "BODY",
         "DECAY", "TIMING", "VARIATION", "OUTPUT", "MIX"
     };
+
+    int padAtPoint(juce::Point<int> point) const;
+    void setDragPad(int newPad);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PhysicalDrumEngineAudioProcessorEditor)
 };
