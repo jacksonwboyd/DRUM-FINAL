30babaab7fb4bebabcd4a8bc1f1dc47623410485
jacksonwboyd <iamjackboyd@gmail.com>
2026-09-05 10:35:25 -0700

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -7,18 +7,29 @@ class PhysicalDrumEngineAudioProcessorEditor final : public juce::AudioProcessor
 public:
     explicit PhysicalDrumEngineAudioProcessorEditor(PhysicalDrumEngineAudioProcessor&);
     ~PhysicalDrumEngineAudioProcessorEditor() override = default;
+
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
     PhysicalDrumEngineAudioProcessor& processor;
-    juce::Label title, subtitle;
-    std::array<std::unique_ptr<juce::TextButton>, PhysicalDrumEngineAudioProcessor::numPads> padButtons;
-    std::array<std::unique_ptr<juce::Label>, PhysicalDrumEngineAudioProcessor::numPads> padLabels;
-    std::array<std::unique_ptr<juce::Slider>, 11> knobs;
-    std::array<std::unique_ptr<juce::Label>, 11> knobLabels;
-    std::array<std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>, 11> attachments;
-    const char* knobIds[11] = {"physicality","transient","attack","brightness","pitch","body","decay","timing","variation","output","mix"};
-    const char* knobNames[11] = {"PHYSICALITY","TRANSIENT","ATTACK","BRIGHTNESS","PITCH","BODY","DECAY","TIMING","VARIATION","OUTPUT","MIX"};
+
+    juce::Label title;
+    juce::Label subtitle;
+    std::array<juce::TextButton, PhysicalDrumEngineAudioProcessor::numPads> padButtons;
+    std::array<juce::Label, PhysicalDrumEngineAudioProcessor::numPads> padLabels;
+    std::array<juce::Slider, 11> knobs;
+    std::array<juce::Label, 11> knobLabels;
+
+    static constexpr std::array<const char*, 11> knobIds = {
+        "physicality", "transient", "attack", "brightness", "pitch", "body",
+        "decay", "timing", "variation", "output", "mix"
+    };
+
+    static constexpr std::array<const char*, 11> knobNames = {
+        "PHYSICALITY", "TRANSIENT", "ATTACK", "BRIGHTNESS", "PITCH", "BODY",
+        "DECAY", "TIMING", "VARIATION", "OUTPUT", "MIX"
+    };
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PhysicalDrumEngineAudioProcessorEditor)
 };
